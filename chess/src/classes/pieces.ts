7264df6baf77c9b808ac3123bfc83ce800d8f7ce
eduard damian garcia <yamiyugi43@gmail.com>
2021-04-17 19:26:58 -0500

    gg

--- chess/src/classes/pieces.ts
+++ chess/src/classes/pieces.ts
@@ -4,13 +4,14 @@ export class ChessBoard{
     mate: boolean;
     move(piece:Piece, x,y){};
     checkMate(){};
+    promotion(){};
 }
 
 export class Piece{
     type: 0 | 1 | 2 | 3 | 4 | 5; // 0:peon, 1: torre, 2: alfil, 3: caballo, 4: reina, 5:rey
     posX: number;
     posY: number;
-    color: 0 | 1;
+    color: 0 | 1; //0 las blancas y 1 las negras
     isLegalMove(x:number, y:number, check: 0 | 1):boolean{
         return false
     };
@@ -18,12 +19,22 @@ export class Piece{
         return false;
     };
     check(cheeseBoard:ChessBoard){}
-    promotion(){};
 }
+
+1/3/5;
 
 class pawn extends Piece {
     constructor(){
-        super()
+        super();
         this.type = 0;
+    }
+}
+
+class tower extends Piece{
+    constructor(posx, posy){
+        super();
+        this.posX = posx;
+        this.posY = posy;
+        this.type = 1;
     }
 }
