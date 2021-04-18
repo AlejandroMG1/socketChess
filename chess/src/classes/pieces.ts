f783d9d2e4a36190bab003f9d295e04abdc34ef9
Alejandro Montoya <alejandrom.montoya@udea.edu.co>
2021-04-17 19:02:20 -0500

    base classes

---
+++ chess/src/classes/pieces.ts
@@ -0,0 +1,29 @@
+export class ChessBoard{
+    board: Piece [][];
+    kings : number[][];
+    mate: boolean;
+    move(piece:Piece, x,y){};
+    checkMate(){};
+}
+
+export class Piece{
+    type: 0 | 1 | 2 | 3 | 4 | 5; // 0:peon, 1: torre, 2: alfil, 3: caballo, 4: reina, 5:rey
+    posX: number;
+    posY: number;
+    color: 0 | 1;
+    isLegalMove(x:number, y:number, check: 0 | 1):boolean{
+        return false
+    };
+    validateMove(x:number, y:number, check: 0 | 1):boolean{
+        return false;
+    };
+    check(cheeseBoard:ChessBoard){}
+    promotion(){};
+}
+
+class pawn extends Piece {
+    constructor(){
+        super()
+        this.type = 0;
+    }
+}
